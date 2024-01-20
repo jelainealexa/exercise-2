f0b8f0ad9a91391918709ee287e2b0ebf611bf00
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-20 19:17:56 +0800

    Print

--- exercise-2.py
+++ exercise-2.py
@@ -1,5 +1,7 @@
 # Print the sum of the current number and the previous number
 
+print("Printing current and previous number sum in a range(10)")
+
 for i in range(10):
     current_number = i
     previous_number = i - 1
