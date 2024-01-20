072b001d3f2baaf983fc260dde8072c70f2598ce
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-21 00:14:51 +0800

    Final (1)

--- exercise-2.py
+++ exercise-2.py
@@ -3,16 +3,17 @@
 # Display message
 print("Printing current and previous number sum in a range(10)")
 
+previous_number = 0
+
 # Iterate the first 10 numbers
 for i in range(10):
     #Assign the current number to variable current_number
     current_number = i
-    
-    #Calculate the previous number by subtracting 1 from the current number
-    previous_number = i - 1
     
     #Calculate the sum of current and previous numbers
     sum = current_number + previous_number
 
     #Display current number, previous number, and their sum
     print(f"Current Number {current_number} Previous Number {previous_number} Sum: {sum}")
+    
+    previous_number = current_number
